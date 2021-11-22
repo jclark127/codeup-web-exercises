8e5748c992d4173aa680bf004e2f17479622d0a8
Jonathan Clark <jonathan.b.clark1127@gmail.com>
2021-11-22 12:34:00 -0600

    string exercises

---
+++ js_exersice.js
@@ -0,0 +1,13 @@
+var sentenceOne = "hello my name is jon clark.\n";
+
+var sentenceTwo = "\tI am in the codeup program for full stack web development.\n";
+
+var sentenceThree = "\t\tI am hoping to get a job as a software developer upon completion of the program.";
+
+console.log(sentenceOne + sentenceTwo + sentenceThree);
+
+var sent = '"can you use quotes without escapes when using a different string container?"';
+
+console.log(sent);
+
+var myName = "Jon";
