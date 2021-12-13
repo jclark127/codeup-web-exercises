94a1b84e44927b5a8f3e7870f6b04e2f88ab23ed
Jonathan Clark <jonathan.b.clark1127@gmail.com>
2021-12-13 14:29:02 -0600

    jquery api and jquery selectors exercise

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,5 +1,11 @@
 "use strict";
+window.onload = function() {
 
-$(document).ready(function() {
-    alert( 'The DOM has finished loading!' );
-});
+    alert($("h1").html());
+
+    $(".codeup").css("border", "solid 1px #FF0000");
+
+    $(".li").css("font-size", "20px");
+
+    $("h1, p, li").css("background-color", "#FFFF00")
+}
