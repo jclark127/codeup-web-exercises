a76e4933c047db5ddb40bef6bbcbfa173eb19aa1
Jonathan Clark <jonathan.b.clark1127@gmail.com>
2022-01-25 09:32:39 -0600

    todays warmup

---
+++ inClass1-25.js
@@ -0,0 +1,17 @@
+"use strict";
+
+
+function duplicates (num) {
+    let stringArr = num.split("");
+    console.log(stringArr);
+    let numObj = {};
+    for (let i = 0; i < stringArr.length; i ++) {
+        if (stringArr[i] === stringArr[i +1]) {
+            let variable = stringArr[i];
+            numObj[]
+        }
+    }
+    return numObj;
+}
+
+console.log(duplicates("2992338111134"));
