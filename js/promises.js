e74147b5704d1caf7418d3104ea2adab2e0ac5c0
Jonathan Clark <jonathan.b.clark1127@gmail.com>
2022-02-08 14:31:04 -0600

    finished first section of promises exercise

--- js/promises.js
+++ js/promises.js
@@ -1,9 +1,15 @@
 'use strict';
 
-function getGithubUsernames() {
-    return fetch('https://api.github.com/',{headers: {'Authorization': GITHUB_TOKEN}})
-        .then(response =>
-            console.log(response.json()));
+function getRecentCommit(user) {
+    return fetch(`https://api.github.com/repos/${user}/codeup-web-exercises/commits`,{headers: {'Authorization': GITHUB_TOKEN}})
+        .then(response => response.json())
+        .then(result =>{
+            let latestCommit = result[0].commit.author.date.substring(0,10)
+            console.log(`Most recent commit for user: ${user} date: ${latestCommit}.`);
+            alert(`Most recent commit for user: ${user} date: ${latestCommit}.`);
+        })
 }
 
-console.log(getGithubUsernames());
+
+let gitUser = "jclark127";
+getRecentCommit(gitUser);
