4feb057ca083cee5be646d97a35b7cb36073aeb8
Jonathan Clark <jonathan.b.clark1127@gmail.com>
2022-02-08 14:56:14 -0600

    finished promises exercise

--- js/promises.js
+++ js/promises.js
@@ -1,10 +1,10 @@
 'use strict';
 
 function getRecentCommit(user) {
-    return fetch(`https://api.github.com/repos/${user}/codeup-web-exercises/commits`,{headers: {'Authorization': GITHUB_TOKEN}})
+    return fetch(`https://api.github.com/repos/${user}/codeup-web-exercises/commits`, {headers: {'Authorization': GITHUB_TOKEN}})
         .then(response => response.json())
-        .then(result =>{
-            let latestCommit = result[0].commit.author.date.substring(0,10)
+        .then(result => {
+            let latestCommit = result[0].commit.author.date.substring(0, 10)
             console.log(`Most recent commit for user: ${user} date: ${latestCommit}.`);
             alert(`Most recent commit for user: ${user} date: ${latestCommit}.`);
         })
@@ -12,4 +12,25 @@ function getRecentCommit(user) {
 
 
 let gitUser = "jclark127";
-getRecentCommit(gitUser);
+
+// getRecentCommit(gitUser);
+
+
+function wait(number) {
+    return new Promise((resolve, reject) => {
+        if (number > 10) {
+            reject("Number is too big.");
+        }
+        setTimeout(() => {
+            if (number < 10) {
+                resolve("Number was reasonable");
+            }
+        }, (number * 1000));
+    });
+}
+
+let number = prompt("Enter a number between 1-10");
+wait(number).then((result) => {
+    console.log(result)
+    alert(`You\'ll see this after ${number} second(s).`);
+}).catch(result => console.log(result));
