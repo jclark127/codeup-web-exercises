710d2dcf274d0b2965987d9e683487797a2f2d3a
Jonathan Clark <jonathan.b.clark1127@gmail.com>
2022-02-17 16:32:21 -0600

    sending unpushed files to git

--- js/inclass2-9.js
+++ js/inclass2-9.js
@@ -1,8 +1,25 @@
 'use strict';
 
-function checkVowel (string){
+function checkVowel(string) {
     let count = string.match(/[aeiou]/gi).length;
     return count;
 }
 
 console.log(checkVowel('How many vowels are in this sentence?'));
+
+let arr = "adsjfdsfsfjsdjfhacabcsbajda";
+let newArr = [];
+
+for (let i = 0; i < arr.length; i++){
+    let count = 0;
+    for (let j = i + 1;j < arr.length; j++) {
+        if (arr[i].includes(arr[j])) {
+            count++;
+            if (count !== 1) {
+                if (!newArr.contains(arr[i]) || newArr[i] === "")
+                newArr.push(`${arr[i]}: ${count}`);
+            }
+        }
+    }
+}
+ console.log(newArr);
